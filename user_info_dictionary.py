8642091d49ecdc4770c90bcd11570408f0f4b688
Marcus Guerrero <marcus.guerrero61@gmail.com>
2024-10-21 13:53:19 +0800

    Making the pseodocode first

---
+++ user_info_dictionary.py
@@ -0,0 +1,9 @@
+#Create a dictionary array for placing multiple entries
+#Place a first loop for another entry by the users if stated yes
+#Make a loop for asking users their name and valid age
+#Print an error statement wherein the maximum characters for the name must not exceed 50 and the age must not exceed the maximum value of 200.
+#Also print an error statement if there is a number inputted in the name and an alphabet for the age
+#If there are no errors, break the loop
+#Generate a command that ask users if they would like to input another entry
+#If they would like to end, break the first loop
+#Create a command that arranges every entry from oldest to youngest when printed.
